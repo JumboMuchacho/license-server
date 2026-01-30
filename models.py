e259d0e997f210921f594248849dd2d68d1a6efb
Jbee <161680711+JumboMuchacho@users.noreply.github.com>
2026-01-30 05:29:59 +0300

    updated the createLicense function to grab values from the new input fields.

--- models.py
+++ models.py
@@ -12,8 +12,8 @@ class License(Base):
     max_devices = Column(Integer, default=1)
     expires_at = Column(DateTime)
     
-    # Using JSON type for automatic serialization
-    assigned_users = Column(JSON, default=list) 
+    # Stores users as a JSON list []
+    assigned_users = Column(JSON, default=list)
 
     devices = relationship("Device", back_populates="license", cascade="all,delete")
 
@@ -23,5 +23,6 @@ class Device(Base):
     id = Column(Integer, primary_key=True)
     license_id = Column(Integer, ForeignKey("licenses.id"), nullable=False)
     device_id = Column(String, nullable=False)
+    last_seen = Column(DateTime, default=datetime.datetime.utcnow)
 
     license = relationship("License", back_populates="devices")
