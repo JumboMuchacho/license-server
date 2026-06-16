77ce2b362e6ebc88a3f83fc22b4430e708d65bda
Jbee <161680711+JumboMuchacho@users.noreply.github.com>
2026-06-16 09:17:43 +0300

    update

--- main.py
+++ main.py
@@ -128,7 +128,7 @@ def get_secure_rules(request: Request, body: dict, x_auth_token: str = Header(..
         db.commit()
     return {
         "isActive": True,
-        "rules": ["//div[contains(@class, 'message')][contains(text(), 'There is no USDT transaction received')]"]
+        "rules": ["//div[contains(@class, 'message')][contains(text(), 'There is no USDT transaction')]"]
     }
 
 @app.post("/api/v1/billing/consume-token")
